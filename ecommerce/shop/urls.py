cfab3a59cd4c7b7f22ffc08fe42b0171aaf81674
joyislam1954 <joyislam1954@gmail.com>
2020-07-09 14:03:41 +0600

    Online Shopping Ecommerce Site

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
-
+from django.urls import path
 app_name = 'shop'
 
 urlpatterns = [
     url(r'^$', views.product_list, name='product_list'),
     url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+    path('search/', views.search, name='search'),
 ]
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -27,3 +27,8 @@ def product_detail(request, id, slug):
         'cart_product_form': cart_product_form,
     }
     return render(request, 'shop/product/detail.html', context)
+
+
+def search(request):
+    products = Product.objects.filter(name__contains=request.GET['title'])
+    return render(request, 'shop/product/search.html', {'products': products})
